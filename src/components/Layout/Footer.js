398803f4ed1f298ce3a6e289f5b9fd59fa1cb8a9
Ahmad Ihsan <ihsanashi@gmail.com>
2021-06-17 18:47:22 +0800

    add content to Footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
-    <footer className='flex flex-row justify-center items-center p-5 relative'>
-      <h5>This is a temporary footer</h5>
+    <footer className='flex flex-col justify-center items-center p-5 relative'>
+      <p>This website was made with...(content from Sanity)</p>
+      <p className='py-7'>Icons here...</p>
+      <small className='text-sm font-light font-sans text-gray-700'>
+        © Ahmad Ihsan, all rights reserved.
+      </small>
     </footer>
   );
 }
