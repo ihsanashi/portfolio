62e15c32c6a50820d2f023501d9ec6b34b658fcd
Ahmad Ihsan <ihsanashi@gmail.com>
2021-06-17 15:40:12 +0800

    add custom document file for setting up google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      sans: ['Barlow', 'sans-serif'],
+      serif: ['Spectral', 'serif'],
+    },
     extend: {},
   },
   variants: {
