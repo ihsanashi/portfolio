80333b9398976594022a16aef6ed19ec0194df9b
Ahmad Ihsan <ihsanashi@gmail.com>
2021-06-17 15:54:06 +0800

    add Layout master component, uupdate homepage to temporary content

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='flex flex-row justify-center items-center p-5 relative'>
+      <h5>This is a temporary footer</h5>
+    </footer>
+  );
+}
---
+++ src/components/Layout/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className='flex flex-row justify-center items-center p-5'>
+      <h5>This is a temporary navbar</h5>
+    </nav>
+  );
+}
