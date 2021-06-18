0929c0045521950ab61a696bd3766d0734e51320
Ahmad Ihsan <ihsanashi@gmail.com>
2021-06-18 19:30:26 +0800

    add homepage hero section

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,34 @@ export default function Home() {
         />
       </Head>
 
+      {/* begin: Layout component */}
       <Layout>
-        <h1 className='text-center'>Homepage content here</h1>
+        {/* begin: main wrapper */}
+        <main>
+          {/* begin: container */}
+          <div className='container'>
+            {/* begin: hero section */}
+            <section className='my-36'>
+              <div className='max-w-xl mx-auto text-center font-sans'>
+                <h6 className='text-xl font-normal text-gray-600'>
+                  Hello there, my name is
+                </h6>
+                <h2 className='text-4xl lg:text-5xl font-bold text-primary-500 mt-3 mb-5'>
+                  Ahmad Ihsan
+                </h2>
+                <h5 className='text-2xl font-normal text-gray-700'>
+                  I'm a Web Developer and UX/UI Designer from Kuala Lumpur,
+                  Malaysia
+                </h5>
+              </div>
+            </section>
+            {/* end: hero section */}
+          </div>
+          {/* end: container */}
+        </main>
+        {/* end: main wrapper */}
       </Layout>
+      {/* end: Layout component */}
     </>
   );
 }
