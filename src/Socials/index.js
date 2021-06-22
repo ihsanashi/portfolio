035b86b4a7371385dd9e982ee1baf6088d2cf017
Ahmad Ihsan <ihsanashi@gmail.com>
2021-06-22 18:06:59 +0800

    add footer socials, fix footer layout

---
+++ src/Socials/index.js
@@ -0,0 +1,25 @@
+import { RiGithubFill, RiLinkedinBoxFill, RiMailFill } from 'react-icons/ri';
+
+const iconSize = 20;
+const iconStyles = 'text-gray-500 group-hover:text-primary-500';
+
+export const Socials = [
+  {
+    id: 'github',
+    title: 'Github',
+    icon: <RiGithubFill size={iconSize} className={iconStyles} />,
+    url: 'https://github.com/ihsanashi',
+  },
+  {
+    id: 'linkedin',
+    title: 'LinkedIn',
+    icon: <RiLinkedinBoxFill size={iconSize} className={iconStyles} />,
+    url: 'https://www.linkedin.com',
+  },
+  {
+    id: 'email',
+    title: 'Email',
+    icon: <RiMailFill size={iconSize} className={iconStyles} />,
+    url: 'mailto:hello@ahmadihsan.com',
+  },
+];
--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,11 +1,54 @@
+import Link from 'next/link';
+import { Sitemap } from '../../Sitemap';
+import { Socials } from '../../Socials';
+
 export default function Footer() {
   return (
-    <footer className='flex flex-col justify-center items-center p-5 relative'>
-      <p>This website was made with...(content from Sanity)</p>
-      <p className='py-7'>Icons here...</p>
-      <small className='text-sm font-light font-sans text-gray-700'>
-        © Ahmad Ihsan, all rights reserved.
-      </small>
+    <footer className='relative flex flex-col items-center justify-center py-16 md:py-24 lg:py-32 bg-gray-50'>
+      <div className='container'>
+        <section className='grid grid-cols-2 gap-5 md:grid-cols-3'>
+          <div className='flex flex-col'>
+            <h6 className='text-base font-light text-gray-600'>Sitemap</h6>
+            <ul className='flex flex-row mt-2 mb-5 list-none'>
+              {Sitemap.map((item) => (
+                <li key={item.id} className='mr-2'>
+                  <Link href={item.link}>
+                    <a className='inline-block font-normal text-gray-500 transition duration-500 ease-in-out transform hover:text-primary-500 hover:scale-95'>
+                      {item.title}
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <small className='font-sans text-sm font-light text-gray-700'>
+              © Ahmad Ihsan, all rights reserved.
+            </small>
+          </div>
+          <p className='text-sm text-center text-gray-700'>
+            This website was built with Next.js and React, the content is stored
+            with Sanity.io, and the data is queried with Apollo and GraphQL.
+          </p>
+          <div>
+            <h6 className='text-base font-light text-gray-600'>Socials</h6>
+            <ul className='flex flex-col mt-2 list-none'>
+              {Socials.map((item) => (
+                <li key={item.id} className='mb-1.5'>
+                  <a
+                    href={item.url}
+                    target='_blank'
+                    className='inline-flex flex-row items-center text-gray-600 group'
+                  >
+                    {item.icon}
+                    <p className='ml-2 text-sm group-hover:text-primary-500'>
+                      {item.title}
+                    </p>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      </div>
     </footer>
   );
 }
