c41ce9969eb22b1e438ab69c7a30e2fc91f61dc3
Ahmad Ihsan <ihsanashi@gmail.com>
2021-06-22 12:57:40 +0800

    add navbar

---
+++ src/Sitemap/index.js
@@ -0,0 +1,22 @@
+export const Sitemap = [
+  {
+    id: 'home',
+    title: 'Home',
+    link: '/',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+    link: '/projects',
+  },
+  {
+    id: 'about',
+    title: 'About',
+    link: '/about',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+    link: '/contact',
+  },
+];
--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,7 +1,48 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Sitemap } from '../../Sitemap';
+
 export default function Navbar() {
   return (
-    <nav className='flex flex-row justify-center items-center p-5'>
-      <h5>This is a temporary navbar</h5>
-    </nav>
+    <div className='sticky top-0 left-0 z-40 flex flex-none w-full py-4 mx-auto bg-white shadow-sm'>
+      <div className='container'>
+        <nav className='flex items-center flex-none'>
+          <div className='flex-none'>
+            <Link href='/'>
+              <a>
+                <Image
+                  src='/ai-logo.svg'
+                  layout='fixed'
+                  height={48}
+                  width={48}
+                />
+              </a>
+            </Link>
+          </div>
+          <div className='flex flex-row items-center flex-auto justify-evenly'>
+            {Sitemap.filter(
+              (item) => !item.title.toLowerCase().includes('contact')
+            ).map((filtered) => (
+              <Link href={filtered.link} key={filtered.id}>
+                <a className='pb-1 font-normal text-gray-700 border-b border-transparent hover:text-primary-500 hover:border-primary-500'>
+                  {filtered.title}
+                </a>
+              </Link>
+            ))}
+          </div>
+          <div className='flex-none'>
+            {Sitemap.filter((item) =>
+              item.title.toLowerCase().includes('contact')
+            ).map((filtered) => (
+              <Link href={filtered.link} key={filtered.id}>
+                <a className='px-5 py-2 bg-white border rounded-md text-primary-500 hover:text-white hover:bg-primary-500 border-primary-500'>
+                  {filtered.title}
+                </a>
+              </Link>
+            ))}
+          </div>
+        </nav>
+      </div>
+    </div>
   );
 }
