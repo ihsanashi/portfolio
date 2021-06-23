f2aa18d2767812fe59cea316918c55e7b6958895
Ahmad Ihsan <ihsanashi@gmail.com>
2021-06-23 10:27:10 +0800

    styling changes and move copyright text to bottom of footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -4,39 +4,40 @@ import { Socials } from '../../Socials';
 
 export default function Footer() {
   return (
-    <footer className='relative flex flex-col items-center justify-center py-16 md:py-24 lg:py-32 bg-gray-50'>
-      <div className='container'>
-        <section className='grid grid-cols-2 gap-5 md:grid-cols-3'>
+    <footer className='relative flex flex-col items-center justify-center py-16 md:py-24 lg:pt-32 lg:pb-20 bg-gray-50'>
+      <div className='container divide-y divide-gray-200'>
+        <section className='grid grid-cols-1 gap-5 pb-12 md:grid-cols-2 lg:grid-cols-3'>
           <div className='flex flex-col'>
             <h6 className='text-base font-light text-gray-600'>Sitemap</h6>
-            <ul className='flex flex-row mt-2 mb-5 list-none'>
+            <ul className='grid grid-cols-3 mt-2 mb-5 list-none'>
               {Sitemap.map((item) => (
-                <li key={item.id} className='mr-2'>
+                <li key={item.id} className='mb-2 mr-2'>
                   <Link href={item.link}>
-                    <a className='inline-block font-normal text-gray-500 transition duration-500 ease-in-out transform hover:text-primary-500 hover:scale-95'>
+                    <a className='inline-block font-normal text-gray-500 transition duration-500 ease-in-out transform hover:text-primary-500'>
                       {item.title}
                     </a>
                   </Link>
                 </li>
               ))}
             </ul>
-            <small className='font-sans text-sm font-light text-gray-700'>
-              © Ahmad Ihsan, all rights reserved.
-            </small>
           </div>
-          <p className='text-sm text-center text-gray-700'>
-            This website was built with Next.js and React, the content is stored
-            with Sanity.io, and the data is queried with Apollo and GraphQL.
-          </p>
+          <div>
+            <h6 className='text-base font-light text-gray-600'>Stack</h6>
+            <p className='mt-2 text-sm font-normal leading-relaxed text-gray-700'>
+              This website was built with Next.js and React, the content is
+              stored with Sanity.io, and the data is queried with Apollo and
+              GraphQL.
+            </p>
+          </div>
           <div>
             <h6 className='text-base font-light text-gray-600'>Socials</h6>
-            <ul className='flex flex-col mt-2 list-none'>
+            <ul className='flex flex-row flex-wrap mt-2 list-none'>
               {Socials.map((item) => (
-                <li key={item.id} className='mb-1.5'>
+                <li key={item.id} className='mr-2'>
                   <a
                     href={item.url}
                     target='_blank'
-                    className='inline-flex flex-row items-center text-gray-600 group'
+                    className='inline-flex flex-row items-center px-3 py-1.5 text-gray-600 transition duration-500 ease-in-out border border-gray-200 rounded-sm group hover:border-primary-500'
                   >
                     {item.icon}
                     <p className='ml-2 text-sm group-hover:text-primary-500'>
@@ -47,6 +48,11 @@ export default function Footer() {
               ))}
             </ul>
           </div>
+        </section>
+        <section className='pt-10'>
+          <small className='block font-sans text-sm font-light text-center text-gray-700'>
+            © Ahmad Ihsan, all rights reserved.
+          </small>
         </section>
       </div>
     </footer>
