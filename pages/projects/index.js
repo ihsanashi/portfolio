0b968171ce9be034001a5a9a6a02acb63157c8db
Ahmad Ihsan <ihsanashi@gmail.com>
2021-06-24 10:37:11 +0800

    adisplay content from sanity

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,7 +1,19 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import Layout from '../../src/components/Layout';
+import { useQuery } from '@apollo/client';
+import { GET_PROJECTS } from '../../lib/queries/allProjectsData';
+import { BiRightArrowAlt } from 'react-icons/bi';
 
 export default function ProjectsPage() {
+  const { loading, error, data } = useQuery(GET_PROJECTS);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  console.log(data);
+
   return (
     <>
       <Head>
@@ -15,17 +27,74 @@ export default function ProjectsPage() {
       {/* begin: Layout component */}
       <Layout>
         <section className='my-36'>
-          <div className='max-w-xl mx-auto'>
-            <div className='container'>
-              <div>
-                <h5 className='font-serif text-xl md:text-2xl font-medium mb-2.5'>
-                  Currently under maintenance 🚧
-                </h5>
-                <p className='font-sans text-base font-normal'>
-                  Will be up shortly
-                </p>
-              </div>
-            </div>
+          <div className='container'>
+            <ul className='grid grid-cols-1 gap-x-5 gap-y-12 md:grid-cols-2 lg:grid-cols-4'>
+              {data.allProject.map((project) => (
+                <li
+                  className='border border-gray-100 rounded-md'
+                  key={project._id}
+                >
+                  <article className='flex flex-col group'>
+                    <div className='relative'>
+                      <Link href={`projects/${project.slug.current}`}>
+                        <a>
+                          <Image
+                            className='rounded-t-md'
+                            src={project.image.asset.url}
+                            layout='responsive'
+                            width={330}
+                            height={200}
+                          />
+                        </a>
+                      </Link>
+                    </div>
+                    <div className='p-5'>
+                      <Link href={`projects/${project.slug.current}`}>
+                        <a>
+                          <h4 className='text-xl font-semibold text-gray-900 transition duration-300 ease-in-out md:text-2xl group-hover:text-primary-500'>
+                            {project.title}
+                          </h4>
+                          <p className='text-sm leading-normal text-gray-600 md:text-base mt-2.5 mb-7.5'>
+                            {project.summary}
+                          </p>
+                        </a>
+                      </Link>
+                      {project.links.length <= 2 && (
+                        <ul className='flex flex-row'>
+                          {project.links.map((item) => (
+                            <li key={item._key}>
+                              <a
+                                href={item.link}
+                                target='_blank'
+                                className={`px-4 py-2 inline-block rounded-md mr-3 font-medium text-base text-center ${
+                                  item.title.toLowerCase() !== 'source code'
+                                    ? 'text-primary-600'
+                                    : 'text-gray-800'
+                                } ${
+                                  item.title.toLowerCase() !== 'source code'
+                                    ? 'bg-primary-50 hover:bg-primary-100 transition duration-500 ease-in-out transform hover:-translate-y-0.5'
+                                    : 'bg-white hover:bg-gray-100 border border-gray-200 transition duration-500 ease-in-out transform hover:-translate-y-0.5'
+                                }`}
+                              >
+                                {item.title}
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                      <div className='flex flex-row items-center justify-start mt-10 group'>
+                        <Link href={`projects/${project.slug.current}`}>
+                          <a className='inline-flex flex-row items-center text-sm font-medium tracking-wider transition duration-500 ease-in-out transform text-primary-600 hover:text-primary-300 hover:translate-x-1'>
+                            Explore
+                            <BiRightArrowAlt className='ml-1.5' size={18} />
+                          </a>
+                        </Link>
+                      </div>
+                    </div>
+                  </article>
+                </li>
+              ))}
+            </ul>
           </div>
         </section>
       </Layout>
