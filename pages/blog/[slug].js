7ec0fba8d6a8f8af737d7dd333eaf3dba9ec7032
Ahmad Ihsan <ihsanashi@gmail.com>
2021-06-25 16:04:59 +0800

    complete layout and content for blog post pages

---
+++ pages/blog/[slug].js
@@ -0,0 +1,97 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Layout from '../../src/components/Layout';
+import { useRouter } from 'next/router';
+import { useQuery } from '@apollo/client';
+import { GET_SINGLE_POST } from '../../lib/queries/singlePostData';
+import moment from 'moment';
+import PortableText from 'react-portable-text';
+import { BiFolderOpen, BiCalendarEdit, BiHash } from 'react-icons/bi';
+
+export default function SinglePostPage() {
+  const router = useRouter();
+
+  const { loading, error, data } = useQuery(GET_SINGLE_POST, {
+    variables: { slug: router.query.slug },
+  });
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  const post = data.allPost.length !== 0 ? data.allPost[0] : null;
+
+  if (post === null) return "Error! Page doesn't exist";
+
+  return (
+    <>
+      <Head>
+        <title>{`${post.title} - Blog | Ahmad Ihsan`}</title>
+        <meta name='description' content={post.description} />
+      </Head>
+
+      {/* begin: Layout component */}
+      <Layout>
+        {/* begin: post content */}
+        <section className='my-36'>
+          <div className='container'>
+            <div className='max-w-xl mx-auto'>
+              <p className='font-sans text-base font-light text-center text-gray-500'>{`Posted ${moment(
+                post.publishedAt
+              ).format('MMMM D, YYYY - h:mm a')}`}</p>
+              <h1 className='font-serif text-4xl font-bold text-center md:text-5xl lg:text-5xl mt-2.5 mb-4'>
+                {post.title}
+              </h1>
+              <h6 className='font-sans text-lg font-normal text-center text-gray-700 md:text-xl'>
+                {post.description}
+              </h6>
+            </div>
+          </div>
+          <div className='relative w-full max-w-4xl py-10 mx-auto'>
+            <Image
+              src={post.cover.asset.url}
+              alt={post.cover.asset.altText}
+              layout='responsive'
+              width={960}
+              height={540}
+              objectFit='cover'
+              className='rounded-md'
+            />
+          </div>
+          <div className='container'>
+            <div className='w-full max-w-5xl mx-auto'>
+              <div className='grid grid-cols-1 gap-5 md:grid-cols-6 lg:grid-cols-12'>
+                <div className='order-2 col-span-1 md:order-1 lg:col-span-3'>
+                  <div className='flex flex-row flex-wrap items-center font-sans text-sm text-gray-600 mb-2.5'>
+                    <BiFolderOpen size={16} />
+                    <p className='font-medium ml-1.5'>{post.category.title}</p>
+                  </div>
+                  <div className='flex flex-row flex-wrap items-center font-sans font-light text-sm text-gray-600 mb-2.5'>
+                    <BiCalendarEdit size={16} />
+                    <p className='ml-1.5'>
+                      {`Last updated ${moment(post._updatedAt).format(
+                        'D MMM YYYY, h:mm a'
+                      )}`}
+                    </p>
+                  </div>
+                  <div className='flex flex-row flex-wrap items-center font-sans font-light text-sm text-gray-600 mb-2.5'>
+                    <BiHash size={16} />
+                    <ul className='flex flex-row flex-wrap ml-1.5'>
+                      {post.tags.map((item, index) => (
+                        <li key={index}>{item}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+                <div className='order-1 col-span-1 lg:col-start-4 lg:col-span-full md:order-2'>
+                  <PortableText content={post.bodyRaw} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* end: post content */}
+      </Layout>
+      {/* end: Layout component */}
+    </>
+  );
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,31 +1,81 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import Layout from '../../src/components/Layout';
+import { useQuery } from '@apollo/client';
+import { GET_POSTS } from '../../lib/queries/allPostsData';
+import moment from 'moment';
+import { BiFolderOpen, BiRightArrowAlt } from 'react-icons/bi';
 
 export default function BlogPage() {
+  const { loading, error, data } = useQuery(GET_POSTS);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
   return (
     <>
       <Head>
         <title>Blog | Ahmad Ihsan</title>
         <meta
           name='description'
-          content="Ahmad Ihsan's blog, currently in the works. Stay tuned!"
+          content="Ahmad Ihsan's writings, mainly focusing on tech, personal or even random topics. Content coming in from Sanity.io."
         />
       </Head>
 
       {/* begin: Layout component */}
       <Layout>
         <section className='my-36'>
-          <div className='max-w-xl mx-auto'>
-            <div className='container'>
-              <div>
-                <h5 className='font-serif text-xl md:text-2xl font-medium mb-2.5'>
-                  Currently under maintenance 🚧
-                </h5>
-                <p className='font-sans text-base font-normal'>
-                  Hope to have this up and running soon
-                </p>
-              </div>
-            </div>
+          <div className='container'>
+            <ul className='grid grid-cols-1 gap-x-5 gap-y-12 md:grid-cols-2 lg:grid-cols-3'>
+              {data.allPost.map((post) => (
+                <li key={post._id}>
+                  <article className='flex flex-col group'>
+                    <div className='relative'>
+                      <Link href={`blog/${post.slug.current}`}>
+                        <a>
+                          <Image
+                            src={post.cover.asset.url}
+                            alt={post.cover.asset.altText}
+                            layout='responsive'
+                            width={330}
+                            height={200}
+                          />
+                        </a>
+                      </Link>
+                    </div>
+                    <div className='flex flex-row flex-wrap items-center my-2.5 font-sans group text-sm'>
+                      <BiFolderOpen className='text-gray-400' size={16} />
+                      <p className='text-gray-600 font-medium ml-1.5'>
+                        {post.category.title}
+                      </p>
+                      <p className='text-gray-300 font-light mx-1.5'>{` • `}</p>
+                      <p className='font-light text-gray-600'>
+                        {moment(post.publishedAt).format('D MMM YYYY, h:mm a')}
+                      </p>
+                    </div>
+                    <div>
+                      <Link href={`blog/${post.slug.current}`}>
+                        <a>
+                          <h4 className='text-xl font-semibold text-gray-900 transition duration-300 ease-in-out md:text-2xl group-hover:text-primary-500'>
+                            {post.title}
+                          </h4>
+                          <div className='flex flex-row flex-wrap items-center group mt-2.5'>
+                            <p className='text-sm leading-normal text-gray-600 md:text-base'>
+                              {post.description}
+                            </p>
+                            <BiRightArrowAlt
+                              className='text-gray-400 border border-transparent rounded-full group-hover:border-primary-500 group-hover:text-primary-500 ml-1.5 transition duration-300 ease-in-out'
+                              size={20}
+                            />
+                          </div>
+                        </a>
+                      </Link>
+                    </div>
+                  </article>
+                </li>
+              ))}
+            </ul>
           </div>
         </section>
       </Layout>
