893b0d2326da2ec5cd828b839033cabcfad33274
Ahmad Ihsan <ihsanashi@gmail.com>
2021-06-30 11:56:51 +0800

    remove gitlab

--- src/Socials/index.js
+++ src/Socials/index.js
@@ -1,9 +1,4 @@
-import {
-  RiGithubFill,
-  RiGitlabFill,
-  RiLinkedinBoxFill,
-  RiMailFill,
-} from 'react-icons/ri';
+import { RiGithubFill, RiLinkedinBoxFill, RiMailFill } from 'react-icons/ri';
 
 const iconSize = 20;
 const iconStyles = 'text-gray-500 group-hover:text-primary-500';
@@ -14,12 +9,6 @@ export const Socials = [
     title: 'Github',
     icon: <RiGithubFill size={iconSize} className={iconStyles} />,
     url: 'https://github.com/ihsanashi',
-  },
-  {
-    id: 'gitlab',
-    title: 'Gitlab',
-    icon: <RiGitlabFill size={iconSize} className={iconStyles} />,
-    url: 'https://gitlab.com/ihsanashi',
   },
   {
     id: 'linkedin',
