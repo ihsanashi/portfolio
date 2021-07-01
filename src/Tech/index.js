60a200747b266e931a45f308b9a3792356b9ec33
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-01 18:14:47 +0800

    add top border for footer, display tech texts instead of logos

---
+++ src/Tech/index.js
@@ -0,0 +1,26 @@
+export const TechStack = [
+  {
+    text: 'Next.js',
+    href: 'https://nextjs.org/',
+  },
+  {
+    text: 'React',
+    href: 'https://reactjs.org/',
+  },
+  {
+    text: 'Tailwind CSS',
+    href: 'https://tailwindcss.com/',
+  },
+  {
+    text: 'Sanity.io',
+    href: 'https://www.sanity.io/',
+  },
+  {
+    text: 'Apollo',
+    href: 'https://www.apollographql.com/',
+  },
+  {
+    text: 'GraphQL',
+    href: 'https://graphql.org/',
+  },
+];
--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -2,22 +2,22 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Sitemap } from '../../Sitemap';
 import { Socials } from '../../Socials';
-import { TechLogos } from '../../TechLogos';
+import { TechStack } from '../../Tech';
 
 export default function Footer() {
   return (
-    <footer className='relative flex flex-col items-center justify-center py-16 md:py-24 lg:pt-32 lg:pb-20 bg-gray-50'>
-      <div className='container divide-y divide-gray-200'>
+    <footer className='relative flex flex-col items-center justify-center py-16 border-t border-gray-200 md:py-24 lg:pt-32 lg:pb-20 bg-gray-50'>
+      <div className='container'>
         <section className='grid grid-cols-1 gap-5 pb-12 md:grid-cols-2 lg:grid-cols-3'>
           <div className='flex flex-col text-sm'>
-            <h6 className='font-light tracking-widest text-gray-600 uppercase'>
+            <p className='text-sm font-medium tracking-wide text-gray-600 uppercase'>
               Sitemap
-            </h6>
+            </p>
             <ul className='grid grid-cols-3 mt-2 mb-5 list-none'>
               {Sitemap.map((item) => (
                 <li key={item.id} className='mb-2 mr-2'>
                   <Link href={item.link}>
-                    <a className='inline-block font-normal text-gray-500 transition duration-500 ease-in-out transform hover:text-primary-500'>
+                    <a className='inline-block text-base font-light text-gray-500 transition duration-500 ease-in-out transform hover:text-primary-500'>
                       {item.title}
                     </a>
                   </Link>
@@ -26,30 +26,27 @@ export default function Footer() {
             </ul>
           </div>
           <div className='order-3 text-sm md:col-span-2 lg:order-2 lg:col-span-1'>
-            <p className='font-light tracking-widest text-gray-600 uppercase'>
+            <p className='text-sm font-medium tracking-wide text-gray-600 uppercase'>
               Built with
             </p>
-            <ul className='grid grid-cols-4 mt-2 gap-y-5'>
-              {TechLogos.map((item, index) => (
-                <li key={index}>
-                  <a href={item.href} target='_blank'>
-                    <Image
-                      layout='fixed'
-                      objectFit='contain'
-                      src={item.src}
-                      alt={item.altText}
-                      height={56}
-                      width={56}
-                    />
+            <ul className='grid grid-cols-3 mt-2 md:grid-cols-5'>
+              {TechStack.map((item, index) => (
+                <li key={index} className='mb-2 mr-2'>
+                  <a
+                    href={item.href}
+                    target='_blank'
+                    className='inline-block font-light text-gray-500 transition duration-500 ease-in-out transform hover:text-primary-500'
+                  >
+                    {item.text}
                   </a>
                 </li>
               ))}
             </ul>
           </div>
           <div className='order-2 text-sm md:order-2 lg:order-3'>
-            <h6 className='font-light tracking-widest text-gray-600 uppercase'>
+            <p className='text-sm font-medium tracking-wide text-gray-600 uppercase'>
               Socials
-            </h6>
+            </p>
             <ul className='flex flex-row flex-wrap mt-2 list-none'>
               {Socials.map((item) => (
                 <li key={item.id} className='mb-2 mr-2'>
@@ -59,7 +56,7 @@ export default function Footer() {
                     className='inline-flex flex-row items-center px-3 py-1.5 text-gray-600 transition duration-500 ease-in-out border border-gray-200 rounded-sm group hover:border-primary-500'
                   >
                     {item.icon}
-                    <p className='ml-2 group-hover:text-primary-500'>
+                    <p className='ml-2 text-gray-500 group-hover:text-primary-500'>
                       {item.title}
                     </p>
                   </a>
@@ -69,9 +66,20 @@ export default function Footer() {
           </div>
         </section>
         <section className='pt-8 md:pt-10'>
-          <div className='flex flex-col flex-wrap items-center justify-between font-sans text-xs font-light text-gray-500 md:flex-row md:text-sm'>
-            <p>© Ahmad Ihsan, all rights reserved.</p>
-            <p className='mt-4 md:mt-0'>Made with 🖤 and ☕</p>
+          <div className='flex flex-row flex-wrap items-start justify-start'>
+            <img
+              className='w-5 h-5 md:h-8 md:w-8'
+              src='/ai-logo.svg'
+              alt='Ahmad Ihsan logo'
+            />
+            <div className='ml-2.5'>
+              <p className='text-sm font-light text-gray-500 pb-1.5'>
+                © Ahmad Ihsan, all rights reserved.
+              </p>
+              <p className='text-xs text-gray-500 font-extralight'>
+                Made with 🖤 and ☕
+              </p>
+            </div>
           </div>
         </section>
       </div>
