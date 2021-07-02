10959c6c829ca31c2b784e1237c877de9540084a
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-02 14:58:28 +0800

    update blog post details styling

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -37,18 +37,16 @@ export default function SinglePostPage() {
       <Layout>
         {/* begin: post content */}
         <section className='my-20 md:my-36'>
-          <div className='container'>
-            <div className='max-w-xl mx-auto'>
-              <p className='font-sans text-base font-light text-center text-gray-500'>{`Posted ${moment(
-                post.publishedAt
-              ).format('MMMM D, YYYY - h:mm a')}`}</p>
-              <h1 className='font-serif text-4xl font-bold text-center md:text-5xl lg:text-5xl mt-2.5 mb-4'>
-                {post.title}
-              </h1>
-              <h6 className='font-sans text-lg font-normal text-center text-gray-700 md:text-xl'>
-                {post.description}
-              </h6>
-            </div>
+          <div className='container max-w-xl mx-auto'>
+            <p className='font-sans text-base font-light text-center text-gray-500'>{`Posted ${moment(
+              post.publishedAt
+            ).format('MMMM D, YYYY - h:mm a')}`}</p>
+            <h1 className='font-serif text-4xl font-bold text-center md:text-5xl lg:text-5xl mt-2.5 mb-4'>
+              {post.title}
+            </h1>
+            <h6 className='font-sans text-lg font-normal text-center text-gray-700 md:text-xl'>
+              {post.description}
+            </h6>
           </div>
           <div className='relative w-full max-w-4xl py-10 mx-auto'>
             <Image
@@ -61,34 +59,32 @@ export default function SinglePostPage() {
               className='rounded-none lg:rounded-md'
             />
           </div>
-          <div className='container'>
-            <div className='w-full max-w-4xl mx-auto'>
-              <div className='grid grid-cols-1 gap-y-8 md:gap-y-16 lg:gap-y-24'>
-                <PortableText
-                  content={post.bodyRaw}
-                  serializers={portableTextSerializer}
-                />
-                <div className='grid grid-cols-1 gap-2.5 lg:gap-5 md:grid-cols-3'>
-                  <div className='flex flex-row flex-wrap items-center font-sans text-sm text-gray-600 mb-2.5'>
-                    <BiFolderOpen size={16} />
-                    <p className='font-medium ml-1.5'>{post.category.title}</p>
-                  </div>
-                  <div className='flex flex-row flex-wrap items-center font-sans font-light text-sm text-gray-600 mb-2.5'>
-                    <BiHash size={16} />
-                    <ul className='flex flex-row flex-wrap ml-1.5'>
-                      {post.tags.map((item, index) => (
-                        <li key={index}>{item}</li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div className='flex flex-row flex-wrap items-center font-sans font-light text-sm text-gray-600 mb-2.5'>
-                    <BiCalendarEdit size={16} />
-                    <p className='ml-1.5'>
-                      {`Updated ${moment(post._updatedAt).format(
-                        'D MMM YYYY, h:mm a'
-                      )}`}
-                    </p>
-                  </div>
+          <div className='container w-full max-w-4xl mx-auto'>
+            <PortableText
+              content={post.bodyRaw}
+              serializers={portableTextSerializer}
+            />
+            <div className='max-w-xs p-5 mt-10 border border-indigo-200 rounded-md md:mt-16 lg:mt-24 bg-indigo-50'>
+              <div className='flex flex-col flex-wrap justify-start'>
+                <div className='flex flex-row flex-wrap items-center font-medium font-sans text-sm text-gray-600 mb-2.5'>
+                  <BiFolderOpen size={16} />
+                  <p className='font-medium ml-1.5'>{post.category.title}</p>
+                </div>
+                <div className='flex flex-row flex-wrap items-center font-sans font-light text-sm text-gray-600 mb-2.5'>
+                  <BiHash size={16} />
+                  <ul className='flex flex-row flex-wrap ml-1.5'>
+                    {post.tags.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div className='flex flex-row flex-wrap items-center font-sans font-light text-sm text-gray-600 mb-2.5'>
+                  <BiCalendarEdit size={16} />
+                  <p className='ml-1.5'>
+                    {`Updated ${moment(post._updatedAt).format(
+                      'D MMM YYYY, h:mm a'
+                    )}`}
+                  </p>
                 </div>
               </div>
             </div>
