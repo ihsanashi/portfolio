233d2c53ee5056c57108785971f12e7b1597cd35
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-02 14:57:43 +0800

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css';
 import '../styles/tailwind.css';
+
+import Head from 'next/head';
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../lib/apolloClient';
 
@@ -7,8 +9,34 @@ export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <Head>
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+        <meta
+          name='viewport'
+          content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
+        />
+        <link
+          href='/favicon/favicon-16x16.png'
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+        />
+        <link
+          href='/favicon/favicon-32x32.png'
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+        />
+        <link
+          rel='apple-touch-icon'
+          href='/favicon/apple-touch-icon.png'
+        ></link>
+        <meta name='theme-color' content='#0C3F7A' />
+      </Head>
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
   );
 }
