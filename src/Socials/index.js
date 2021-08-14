df68f7759e17b189f1f8563e89cede92b77915e4
Ahmad Ihsan <ihsanashi@gmail.com>
2021-08-14 19:30:31 +0800

    fix linkedin link

--- src/Socials/index.js
+++ src/Socials/index.js
@@ -15,7 +15,7 @@ export const Socials = [
     id: 'linkedin',
     title: 'LinkedIn',
     icon: <RiLinkedinBoxFill size={iconSize} className={iconStyles} />,
-    url: 'https://www.linkedin.com',
+    url: 'https://www.linkedin.com/in/ihsanashi/',
   },
   {
     id: 'email',
