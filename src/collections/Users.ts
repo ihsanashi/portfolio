d27208012a1aa25af618fe21e2b6a7883af5e2f8
Ahmad Ihsan <ihsanashi@gmail.com>
2025-04-20 22:14:43 +0800

    feat: update Users collection

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,13 +1,26 @@
-import type { CollectionConfig } from 'payload'
+import { authenticated } from '@/access';
+
+import type { CollectionConfig } from 'payload';
 
 export const Users: CollectionConfig = {
   slug: 'users',
+  access: {
+    admin: authenticated,
+    create: authenticated,
+    delete: authenticated,
+    update: authenticated,
+    read: authenticated,
+  },
   admin: {
-    useAsTitle: 'email',
+    defaultColumns: ['name', 'email'],
+    useAsTitle: 'name',
   },
   auth: true,
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      name: 'name',
+      type: 'text',
+    },
   ],
-}
+  timestamps: true,
+};
