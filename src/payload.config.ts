8f792129c67f28390a130d89f47e126ac096b230
Ahmad Ihsan <ihsanashi@gmail.com>
2025-04-20 23:07:34 +0800

    feat: generate payload types

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,17 +1,18 @@
 // storage-adapter-import-placeholder
-import { postgresAdapter } from '@payloadcms/db-postgres'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
-import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
-import sharp from 'sharp'
+import { postgresAdapter } from '@payloadcms/db-postgres';
+import { payloadCloudPlugin } from '@payloadcms/payload-cloud';
+import { lexicalEditor } from '@payloadcms/richtext-lexical';
 
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
+import path from 'path';
+import { buildConfig } from 'payload';
+import sharp from 'sharp';
+import { fileURLToPath } from 'url';
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
+import { Media } from './collections/Media';
+import { Users } from './collections/Users';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default buildConfig({
   admin: {
@@ -36,4 +37,4 @@ export default buildConfig({
     payloadCloudPlugin(),
     // storage-adapter-placeholder
   ],
-})
+});
