cdf0ab477fbb63584ca6a1b0b4cef20ba45dc3ab
Ahmad Ihsan <ihsanashi@gmail.com>
2025-04-21 08:05:40 +0800

    feat: add payload collection configs, use default lexical editor

---
+++ src/collections/Pages/index.ts
@@ -0,0 +1,128 @@
+import { authenticated, authenticatedOrPublished } from '@/access';
+import { Archive } from '@/blocks/ArchiveBlock/config';
+import { CallToAction } from '@/blocks/CallToAction/config';
+import { Content } from '@/blocks/Content/config';
+import { FormBlock } from '@/blocks/Form/config';
+import { MediaBlock } from '@/blocks/MediaBlock/config';
+import { slugField } from '@/fields/slug';
+import { hero } from '@/heros/config';
+import { generatePreviewPath } from '@/utilities/generatePreviewPath';
+import {
+  MetaDescriptionField,
+  MetaImageField,
+  MetaTitleField,
+  OverviewField,
+  PreviewField,
+} from '@payloadcms/plugin-seo/fields';
+
+import { CollectionConfig } from 'payload';
+
+export const Pages: CollectionConfig<'pages'> = {
+  slug: 'pages',
+  access: {
+    create: authenticated,
+    update: authenticated,
+    delete: authenticated,
+    read: authenticatedOrPublished,
+  },
+  defaultPopulate: {
+    title: true,
+    slug: true,
+  },
+  admin: {
+    defaultColumns: ['title', 'slug', 'updatedAt'],
+    livePreview: {
+      url: ({ data, req }) => {
+        const path = generatePreviewPath({
+          slug: typeof data?.slug === 'string' ? data.slug : '',
+          collection: 'pages',
+          req,
+        });
+
+        return path;
+      },
+    },
+    preview: (data, { req }) =>
+      generatePreviewPath({
+        slug: typeof data?.slug === 'string' ? data.slug : '',
+        collection: 'pages',
+        req,
+      }),
+    useAsTitle: 'title',
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      type: 'tabs',
+      tabs: [
+        {
+          fields: [hero],
+          label: 'Hero',
+        },
+        {
+          fields: [
+            {
+              name: 'layout',
+              type: 'blocks',
+              blocks: [CallToAction, Content, MediaBlock, Archive, FormBlock],
+              required: true,
+              admin: {
+                initCollapsed: true,
+              },
+            },
+          ],
+          label: 'Content',
+        },
+        {
+          name: 'meta',
+          label: 'SEO',
+          fields: [
+            OverviewField({
+              titlePath: 'meta.title',
+              descriptionPath: 'meta.description',
+              imagePath: 'meta.image',
+            }),
+            MetaTitleField({
+              hasGenerateFn: true,
+            }),
+            MetaImageField({
+              relationTo: 'media',
+            }),
+
+            MetaDescriptionField({}),
+            PreviewField({
+              // if the `generateUrl` function is configured
+              hasGenerateFn: true,
+
+              // field paths to match the target field for data
+              titlePath: 'meta.title',
+              descriptionPath: 'meta.description',
+            }),
+          ],
+        },
+      ],
+    },
+    {
+      name: 'publishedAt',
+      type: 'date',
+      admin: {
+        position: 'sidebar',
+      },
+    },
+    ...slugField(),
+  ],
+  hooks: {},
+  versions: {
+    drafts: {
+      autosave: {
+        interval: 100,
+      },
+      schedulePublish: true,
+    },
+    maxPerDoc: 50,
+  },
+};
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -12,6 +12,8 @@ import { Footer } from './Footer/config';
 import { Header } from './Header/config';
 import { Categories } from './collections/Categories';
 import { Media } from './collections/Media';
+import { Pages } from './collections/Pages';
+import { Posts } from './collections/Posts';
 import { Users } from './collections/Users';
 
 const filename = fileURLToPath(import.meta.url);
@@ -24,8 +26,8 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
-  editor: lexicalEditor(),
+  collections: [Categories, Media, Pages, Posts, Users],
+  editor: defaultLexical,
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
