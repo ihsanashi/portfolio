63134ec32bca3d16d5dc469b3685707933d99d5b
Ahmad Ihsan <ihsanashi@gmail.com>
2025-04-21 08:06:18 +0800

    feat: add payload blocks

---
+++ src/blocks/Form/Message/index.tsx
@@ -0,0 +1,14 @@
+import RichText from '@/components/RichText';
+import { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical';
+
+import React from 'react';
+
+import { Width } from '../Width';
+
+export const Message: React.FC<{ message: SerializedEditorState }> = ({ message }) => {
+  return (
+    <Width className='my-12' width='100'>
+      {message && <RichText data={message} />}
+    </Width>
+  );
+};
