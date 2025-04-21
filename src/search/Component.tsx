6438045939e5e5b7553597a84d8932220c6f82bf
Ahmad Ihsan <ihsanashi@gmail.com>
2025-04-21 08:02:00 +0800

    feat: add search component

---
+++ src/search/Component.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useDebounce } from '@/utilities/useDebounce';
+
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
+export const Search: React.FC = () => {
+  const [value, setValue] = useState('');
+  const router = useRouter();
+
+  const debouncedValue = useDebounce(value);
+
+  useEffect(() => {
+    router.push(`/search${debouncedValue ? `?q=${debouncedValue}` : ''}`);
+  }, [debouncedValue, router]);
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <Label htmlFor='search' className='sr-only'>
+          Search
+        </Label>
+        <Input
+          id='search'
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+          placeholder='Search'
+        />
+        <button type='submit' className='sr-only'>
+          submit
+        </button>
+      </form>
+    </div>
+  );
+};
