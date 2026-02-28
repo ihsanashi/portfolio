2f6b7a24f67e459262e17f47fb2d0fa451a5e4cd
Ahmad Ihsan <ihsanashi@gmail.com>
2026-02-28 18:21:10 +0800

    build: add configs to deploy via Cloudflare Workers

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,4 +13,5 @@ export default defineConfig({
   },
 
   integrations: [react()],
+  adapter: cloudflare(),
 });
